5803703208ab54d25ebdfb0667191d6f79f9f8ee
fgiger <florian.giger@gris.informatik.tu-darmstadt.de>
2018-12-29 09:35:28 +0100

    Adding Parcel and making tictactoe game class

---
+++ src/js/game/game.js
@@ -0,0 +1,78 @@
+/**
+ * A class that simulates a Tic Tac Toe Game
+ */
+class tictactoegame {
+  constructor() {
+    this.field = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    this.roundsPlayed = 0;
+  }
+
+  /**
+   * Gets the current State of the Playing Field
+   * @returns An array of 9 representing the playing field
+   */
+  getStandings() {
+    return this.field;
+  }
+
+  /**
+   * Indicates the number of rounds played on this game already
+   * @returns the number of rounds played
+   */
+  getRoundsPlayed() {
+    return this.roundsPlayed;
+  }
+
+  /**
+   * Determines if a game has been won
+   * @returns the player who has won (1 or 2) or 0 if its not won yet
+   */
+  getWinner() {
+    const winningStandings = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+
+    let winner = 0;
+
+    if (this.roundsPlayed < 4) {
+      return 0;
+    }
+
+    winningStandings.forEach(element => {
+      if (
+        this.field[element[0]] !== 0 &&
+        this.field[element[0]] === this.field[element[1]] &&
+        this.field[element[1]] === this.field[element[2]]
+      ) {
+        winner = this.field[element[0]];
+      }
+    });
+
+    return winner;
+  }
+
+  /**
+   * A single move in the tictactoe game
+   * @param {*} player the number of the player making the move
+   * @param {*} position The Field the player wants to put his sign in (number 0-8)
+   * @returns if the move was legal (true) or not (false)
+   */
+  makeMove(player, position) {
+    if (this.field[position] === 0) {
+      this.field[position] = player;
+      this.roundsPlayed++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+export default tictactoegame;
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,23 @@
-console.log("Hallo");
+import tictactoe from "./game/game";
+
+console.log("Starting Tic Tac Toe...");
+
+const game1 = new tictactoe();
+console.log(game1.getStandings());
+
+game1.makeMove(1, 4);
+console.log("Spieler 1 setzt auf 4");
+console.log(game1.getStandings());
+game1.makeMove(2, 3);
+console.log("Spieler 2 setzt auf 3");
+console.log(game1.getStandings());
+game1.makeMove(1, 1);
+console.log("Spieler 1 setzt auf 1");
+console.log(game1.getStandings());
+game1.makeMove(2, 0);
+console.log("Spieler 2 setzt auf 0");
+console.log(game1.getStandings());
+game1.makeMove(1, 7);
+console.log("Spieler 1 setzt auf 7");
+console.log(game1.getStandings());
+console.log("Gewinner ist: Spieler " + game1.getWinner());
